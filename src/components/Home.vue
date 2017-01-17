06db8db1eae767e21ff227925d9852203d9a7530
Alec <insanealec@gmail.com>
2017-01-17 18:14:59 -0500

    Use firebase for home page Blogs will go here someday. Next I need to make some page that I can use to edit markdown, won't be hard with my current plugins, should use firebase auth somehow.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,35 @@
 <template>
-	<div class="card">
+<div class="wide">
+	<div v-for="p in posts" class="card">
 		<div class="card-header">
-			Welcome!
+			{{p.title}}
 		</div>
 		<div class="card-block">
-			<p class="card-text">Maybe blog stuff will go here, I have no idea.</p>
+			<p v-html="compiledMarkdown(p.body)" class="card-text"></p>
 		</div>
 	</div>
+</div>
 </template>
 
 <script>
-
+export default {
+	mounted: function() {
+		var self = this;
+		database.ref('/posts').once('value').then(function(snapshot) {
+			self.posts = snapshot.val();
+		});
+	},
+	methods: {
+		compiledMarkdown: function(md) {
+			return marked(md, {});
+		}
+	},
+	data: function() {
+		return {
+			posts: [],
+		}
+	}
+}
 </script>
 
 <style>
