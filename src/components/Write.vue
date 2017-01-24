ed446802c12aadfa5351ade1fb2b96bd484d5ae0
Alec <insanealec@gmail.com>
2017-01-24 18:49:22 -0500

    Switch between writing posts and projects - Load posts and projects from the DB also, but don't use them yet. - Save new and overwrite old posts and projects easily with the editor.

--- src/components/Write.vue
+++ src/components/Write.vue
@@ -4,12 +4,28 @@
 		Title:&nbsp;
 		<input v-model="title">
 		<button @click="switchMode()" class="preview">Preview Markdown</button>
+		<select v-model="currType">
+			<option v-for="t in types">{{t}}</option>
+		</select>
 	</div>
 
 	<textarea v-show="!previewMode" v-model="body"></textarea>
 	<div v-show="previewMode" v-html="compiledMarkdown"></div>
 
-	<button @click="saveContent" class="btn btn-primary">Save</button>
+	<div v-if="currType=='projects'" class="project-group">
+		<div class="form-group">
+			<label for="linkText">Link Text</label>
+			<input v-model="linkText" id="linkText" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="link">Link</label>
+			<input v-model="link" id="link" class="form-control">
+		</div>
+	</div>
+
+	<div class="card-footer text-right">
+		<button @click="saveContent" class="btn btn-primary">Save</button>
+	</div>
 </div>
 </template>
 
@@ -19,25 +35,54 @@ export default {
 		return {
 			title: "",
 			body: "",
+			linkText: "",
+			link: "",
 			previewMode: false,
+			posts: [],
+			projects: [],
+			types: [
+				"posts",
+				"projects",
+			],
+			currType: "posts",
 		}
 	},
 	methods: {
 		saveContent: function() {
-			database.ref('posts/'+this.title).set({
+			//Everything should have a title and body.
+			var options = {
 				title: this.title,
-				body: this.body,
-			});
+				body: this.body
+			};
+
+			//I don't like it, but add an if for each new type added.
+			if(this.currType === "projects")
+			{
+				options.link = this.link;
+				options.linkText = this.linkText;
+			}
+
+			//Using the current type, add this item with the necessary options.
+			database.ref(this.currType+'/'+this.title).set(options);
 		},
 		switchMode: function() {
+			//Each switch sets an inverse of itself.
 			this.previewMode = !this.previewMode;
 		}
 	},
 	mounted: function() {
-		//Load posts for editing?
+		//Load posts and projects for editing.
+		var self = this;
+		database.ref('posts').once('value').then(function(snapshot) {
+			self.posts = snapshot.val();
+		});
+		database.ref('projects').once('value').then(function(snapshot) {
+			self.projects = snapshot.val();
+		});
 	},
 	computed: {
 		compiledMarkdown: function() {
+			//Each preview mode toggle shows this computed prop.
 			return marked(this.body, {});
 		}
 	},
